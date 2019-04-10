3f702ce694b7b7bd6df77a60cd6578a8e1744bb5
杨瑞 <1341672184@qq.com>
2019-04-10 15:46:41 +0800

    xiugai

--- script.groovy
+++ script.groovy
@@ -2,8 +2,8 @@ import java.util.regex.Matcher
 
 //修改端口
 //run(new File("scripts/changePorts.groovy"),["80","443","8001","8002"] as String[])
-
+run(new File("scripts/changePorts.groovy"),["9901","4432","8005","8006"] as String[])
 //发行版
-run(new File("scripts/pack.groovy"))
+//run(new File("scripts/pack.groovy"))
 //run(new File("scripts/deleteClasses.groovy"))
 
